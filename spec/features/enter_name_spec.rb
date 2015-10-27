8cf247e1ec5369e2acadad911f038075cbd9f3a6
jbhdeconinck <jbhdeconinck@gmail.com>
2015-10-27 17:36:16 +0000

    ch20

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,12 +2,28 @@ require './app'
 # As two Players,
 # So we can play a personalised game of Battle,
 # We want to Start a fight by entering our names and seeing them
-feature 'Enter names' do
-  scenario 'submitting names' do
+describe Battle do
+  before(:each) do
     visit('/')
     fill_in(:player_1_name, with: 'Rajeev')
     fill_in(:player_2_name, with: 'Julien')
     click_button('Submit')
-    expect(page).to have_content 'Rajeev vs. Julien'
   end
+
+  feature 'Enter names' do
+    scenario 'submitting names' do
+      expect(page).to have_content 'Rajeev vs. Julien'
+    end
+  end
+
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+
+  feature 'How close to winning?' do
+    scenario 'see hit points' do
+      expect(page).to have_content 'Julien: 60HP'
+    end
+  end
+
 end
